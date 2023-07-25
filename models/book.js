ff0a573d21ad833b35bf13debe31ed0d73b00b5b
Aady <aditya.psinghciv22@iitbhu.ac.in>
2023-07-26 01:07:55 +0530

    Fist commit

--- models/book.js
+++ models/book.js
@@ -1,6 +1,18 @@
 var mongoose=require("mongoose");
 //DEFINING THE BOOK MODEL
+
+
 var bookSchema=new mongoose.Schema({
+    title:String,
+    genre:String,
+    author:String,
+    rating:{
+        type:Number,
+        max:5,
+        min:1
+    },
+    mrp:Number,
+    available_copies:Number
 	/*TODO: DEFINE the following attributes-
     title, genre, author, description, rating (out of 5), mrp, available_copies(instances).
      */
