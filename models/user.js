619e8d91609ac700e5a4fb95af53f196ceb6afb4
JayeshSChaudhari <chaudhari.jayesh.cd.mec22@itbhu.ac.in>
2023-07-31 19:47:52 +0530

    Some files

--- models/user.js
+++ models/user.js
@@ -1,14 +1,20 @@
-var mongoose=require("mongoose");
-var	passportLocal=require("passport-local-mongoose");
+var mongoose = require("mongoose");
+var passportLocal = require("passport-local-mongoose");
 //DEFINING THE USER MODEL
-var userSchema=new mongoose.Schema({
+var userSchema = new mongoose.Schema({
 
-	//TODO: DEFINE USERNAME AND PASSSWORD ATTRIBUTES
+    //TODO: DEFINE USERNAME AND PASSSWORD ATTRIBUTES
 
+    USERNAME: String,
+    PASSWORD: String,
 
-    loaned_books:[
+    loaned_books: [
         //TODO: embed reference to id's of book copies loaned by this particular user in this array
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Book"
+        }
     ]
 })
 userSchema.plugin(passportLocal);
-module.exports=mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
